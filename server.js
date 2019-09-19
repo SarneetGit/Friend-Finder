3cee7aa6eedacb3fde0a777060889287a23f39c2
Sarneet Longia <sarneet.longia@gmail.com>
2019-09-18 20:33:19 -0400

    mysql syntax error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
+    connection = sql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = sql.createConnection({
         host: "localhost",
